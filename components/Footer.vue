80b11b6de1ca1cfb64631658f09251bc1098b24d
alexgil1994 <alexgil2000@hotmail.com>
2021-03-07 14:29:05 +0200

    Initial commit

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-end mr-4 pb-3">
+    <h3>All rights Reserved</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen bg-gray-300">
+    <Navbar/>
+    <!-- Pages -->
+    <Nuxt />
+    <Footer/>
+    <!-- Global Notification -->
+    <Notification/>
+  </div>
+</template>
+
+<style>
+</style>
