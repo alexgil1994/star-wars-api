c5f9a06d2dee2bd11df766f95c3bb4086001214f
alexgil1994 <alexgil2000@hotmail.com>
2021-03-08 19:42:41 +0200

    Fixed title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'WeatherApp',
+    title: 'Star Wars Api',
     htmlAttrs: {
       lang: 'en'
     },
